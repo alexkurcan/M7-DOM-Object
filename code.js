1faa688428129fb03340f17b68e298f4341dc3b1
Local Student Account <587user@H0587LSTUDP5L8.ad.wcpss.net>
2025-03-14 12:46:23 -0400

    third commit, removing CSS adding some lines

--- code.js
+++ code.js
@@ -2,17 +2,15 @@ const myDiv = document.getElementById("myDiv");
 const heading = document.createElement("h1");
 heading.textContent = "Welcome to DOM homework";
 heading.classList.add("highlight");
-
 const paragraph = document.createElement("p");
 paragraph.textContent = "This is your first DOM homework assignment";
-
 const ul = document.createElement("ul");
+
 ["Item 1", "Item 2", "Item 3"].forEach(text => {
     const li = document.createElement("li");
     li.textContent = text;
     ul.appendChild(li);
 });
-
 myDiv.append(heading, paragraph, ul);
 let count = 1;
 document.getElementById("addItemButton").addEventListener("click", () => {
@@ -21,11 +19,9 @@ document.getElementById("addItemButton").addEventListener("click", () => {
     li.style.color = getRandomColor();
     ul.appendChild(li);
 });
-
 myDiv.addEventListener("click", () => {
     myDiv.style.backgroundColor = getRandomColor();
 });
-
 function getRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
